9295e61b0e2ca5b7985cb155cd6451d03507e610
Artur Buivydis <buivya11@gmail.com>
2025-07-15 17:14:49 +0200

    Mittweida Project

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter basename="/Mittweida">
-            <App/>
+        <BrowserRouter>
+        <App/>
         </BrowserRouter>
     </React.StrictMode>
 );
