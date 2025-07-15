0a2cfef078db20b4b54999e7513a0ee2d9d8885c
Artur Buivydis <buivya11@gmail.com>
2025-07-15 17:25:34 +0200

    Mittweida Project

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -8,7 +8,7 @@ const Welcome = () => {
     const navigate = useNavigate();
 
     const handleUpload =() =>{
-        navigate('/CreateAccount')
+        navigate('/createAccount')
     }
 
     return (
